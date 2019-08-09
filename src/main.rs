f26f5d13b5df9172a2cbe0af208c57e116929c68
Erich Schroeter <erichschroeter@gmail.com>
2019-08-09 14:50:26 -0500

    winapi listing visible windows

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 #[cfg(windows)] extern crate winapi;
 use std::io::Error;
+use winapi::shared::minwindef::LPARAM;
+use winapi::shared::windef::HWND;
+use winapi::shared::ntdef::WCHAR;
+// use winapi::shared::ntdef::LPWSTR;
+use winapi::um::winuser::EnumWindows;
+use winapi::um::winuser::GetWindowTextW;
+use winapi::um::winuser::IsWindowVisible;
 
 #[cfg(windows)]
 fn print_message(msg: &str) -> Result<i32, Error> {
@@ -22,6 +29,23 @@ fn print_message(msg: &str) -> Result<(), Error> {
     Ok(())
 }
 
+#[cfg(windows)]
+unsafe extern "system" fn window_info_callback(
+    hwnd: HWND,
+    _l_param: LPARAM
+) -> i32 {
+    let mut text: [WCHAR; 64] = [0; 64];
+    let is_visible = IsWindowVisible(hwnd) != 0;
+    if is_visible {
+        GetWindowTextW(hwnd, text.as_mut_ptr(), 64);
+        println!("{:?}: {}", hwnd, String::from_utf16(&text).unwrap());
+    }
+    1
+}
+
 fn main() {
+    unsafe {
+        EnumWindows(Some(window_info_callback), 5);
+    }
     print_message("Hello, world!").unwrap();
 }
