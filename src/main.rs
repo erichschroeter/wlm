7eab9ca365c4b8702f64ecddf2c1d20964ae3e08
Erich Schroeter <erichschroeter@gmail.com>
2019-08-09 20:46:39 -0500

    filter out explorer.exe that doesn't have text

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #[cfg(windows)] extern crate winapi;
 use std::io::Error;
+use std::path::Path;
+use std::ffi::OsStr;
 use winapi::shared::minwindef::LPARAM;
 use winapi::shared::minwindef::DWORD;
 use winapi::shared::minwindef::MAX_PATH;
 use winapi::shared::minwindef::HINSTANCE;
+use winapi::shared::windef::RECT;
 use winapi::shared::windef::HWND;
 use winapi::shared::ntdef::WCHAR;
 use winapi::shared::ntdef::NULL;
@@ -11,6 +14,7 @@ use winapi::um::winuser::EnumWindows;
 use winapi::um::winuser::GetWindowTextW;
 use winapi::um::winuser::IsWindowVisible;
 use winapi::um::winuser::GetWindowThreadProcessId;
+use winapi::um::winuser::GetWindowRect;
 use winapi::um::winnt::HANDLE;
 use winapi::um::winnt::{PROCESS_QUERY_INFORMATION, PROCESS_VM_READ};
 use winapi::um::processthreadsapi::OpenProcess;
@@ -48,12 +52,53 @@ unsafe extern "system" fn window_info_callback(
     if is_visible {
         let mut proc_id: DWORD = 0;
         let mut module_name: [WCHAR; MAX_PATH] = [0; MAX_PATH];
+        let mut dimensions = RECT { left: 0, top: 0, right: 0, bottom: 0 };
+        let dimptr = &mut dimensions as *mut RECT;
         GetWindowTextW(hwnd, text.as_mut_ptr(), 64);
+        let mut text = text.to_vec();
+        if let Some(first) = text.iter().position(|&b| b == 0) {
+            text.truncate(first);
+        }
         GetWindowThreadProcessId(hwnd, &mut proc_id);
+        GetWindowRect(hwnd, dimptr);
         let process_handle: HANDLE = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, 0, proc_id);
         GetModuleFileNameExW(process_handle, NULL as HINSTANCE, module_name.as_mut_ptr(), MAX_PATH as u32);
         CloseHandle(process_handle);
-        println!("{:?}: {} ------- {}", hwnd, String::from_utf16(&text).unwrap(), String::from_utf16(&module_name).unwrap());
+        let mut module_name = module_name.to_vec();
+        if let Some(first) = module_name.iter().position(|&b| b == 0) {
+            module_name.truncate(first)
+        }
+        let process_name = String::from_utf16(&module_name).unwrap();
+        let process_path = Path::new(&process_name);
+        let process_name = match process_path.file_name() {
+            Some(path) => {
+                path.to_str().unwrap()
+            },
+            None => { "" }
+        };
+        if process_name == "explorer.exe" {
+            if !text.is_empty() {
+                println!("[{:?}] {}\n\t{}\n\t({}, {})\n\t{}, {}",
+                    hwnd,
+                    String::from_utf16(&text).unwrap(),
+                    String::from_utf16(&module_name).unwrap(),
+                    dimensions.top,
+                    dimensions.bottom,
+                    dimensions.left,
+                    dimensions.right
+                );
+            }
+        } else {
+            println!("[{:?}] {}\n\t{}\n\t({}, {})\n\t{}, {}",
+                hwnd,
+                String::from_utf16(&text).unwrap(),
+                String::from_utf16(&module_name).unwrap(),
+                dimensions.top,
+                dimensions.bottom,
+                dimensions.left,
+                dimensions.right
+            );
+        }
     }
     1
 }
@@ -62,5 +107,4 @@ fn main() {
     unsafe {
         EnumWindows(Some(window_info_callback), 5);
     }
-    print_message("Hello, world!").unwrap();
 }
