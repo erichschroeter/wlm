bd641bbc697c5ea98293d7dd7a29d2e5588f0b63
Erich Schroeter <erichschroeter@gmail.com>
2020-05-03 19:46:23 -0500

    WIP enum monitors winapi

--- src/platform/win.rs
+++ src/platform/win.rs
@@ -25,6 +25,40 @@ use winapi::{
 		},
 	},
 };
+use std::ptr;
+
+unsafe extern "system" fn monitor_enum_proc(
+    hmonitor: HMONITOR,
+    _hdc: HDC,
+    _place: LPRECT,
+    data: LPARAM,
+) -> BOOL {
+    let monitors = data as *mut Vec<Monitor>;
+    (*monitors).push(Monitor::new(hmonitor));
+    TRUE // continue enumeration
+}
+
+pub fn list_monitors() -> Vec<Monitor> {
+    let mut monitors: Vec<Monitor> = Vec::new();
+    unsafe {
+        winuser::EnumDisplayMonitors(
+            ptr::null_mut(),
+            ptr::null_mut(),
+            Some(monitor_enum_proc),
+            &mut monitors as *mut _ as LPARAM,
+        );
+    }
+    monitors
+}
+
+#[derive(Debug)]
+pub struct Monitor(HMONITOR);
+
+impl Monitor {
+	pub(crate) fn new(hmonitor: HMONITOR) -> Self {
+		Monitor(hmonitor)
+	}
+}
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct WindowState {
